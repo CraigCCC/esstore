ddad9e518cb50acec7e0f28626a8c29a2723a3ea
craig <craigya89@gmail.com>
2020-02-14 11:47:49 +0800

    View helper - cover_image_tag:

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,4 +3,28 @@ module ProductsHelper
     direction = params[:direction] == 'DESC' ? 'ASC' : 'DESC'
     link_to '價格', order_by: column, direction: direction
   end
+
+  def cover_image_tag(product, width, height, multiple_image: false)
+    if multiple_image
+      multiple_cover_image(product, width, height)
+    elsif product.images.attached?
+      simple_cover_image(product, width, height)
+    else
+      fake_image(width, height)
+    end
+  end
+
+  private
+
+  def multiple_cover_image(product, width, height)
+    image_tag product.variant(resize_to_fill: ["#{width}", "#{height}"])
+  end
+
+  def simple_cover_image(product, width, height)
+    image_tag product.images.first.variant(resize_to_fill: ["#{width}", "#{height}"])
+  end
+
+  def fake_image(width, height)
+    image_tag "https://fakeimg.pl/#{width}x#{height}/?text=upload image!"
+  end
 end
